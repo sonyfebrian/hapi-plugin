98bb399d03e26665740ba7226f8b807290d61f75
s <s@localhost.localdomain>
2021-06-26 21:29:29 +0700

    data validation implemented

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const routes = require('./api/notes/routes');
 const notes = require('./api/notes');
 const NotesService = require('./service/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
   const notesService = new NotesService();
@@ -20,6 +21,7 @@ const init = async () => {
     plugin: notes,
     options: {
       service: notesService,
+      validator: NotesValidator,
     },
   });
 
